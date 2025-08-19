2bf4b3f7b868049f6e988c03a9b15841ca78b40a
Harman khurana <work.harmankhurana@gmail.com>
2025-08-19 14:33:26 +0530

    Cohort 3 100xdevs

---
+++ Week 6 - Authentication and JWT/index.js
@@ -0,0 +1,13 @@
+// Creating my own authentication(auth) code
+const express = require("express");
+const app = express();
+
+app.post("/signup" , function (req,res){
+
+});
+
+app.post("/signin" , function(req,res){
+    
+});
+
+app.listen(3000);
